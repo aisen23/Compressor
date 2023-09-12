d74ac45bd891504f4c187d9a4f4dbde753c91316
Aisen Burnashev <oruzejnik@gmail.com>
2023-09-12 18:51:07 +0700

    Added threads pool logic to Compressor class

--- src/Compressor.cpp
+++ src/Compressor.cpp
@@ -3,12 +3,37 @@
 #include "Compressor.h"
 #include "huffman/HuffmanCompressorImpl.h"
 
-std::vector<uint8_t> ai::Compressor::Compress(const std::vector<int>& array) const {
-    HuffmanCompressorImpl impl;
+ai::CompressorImpl::CompressorImpl(Compressor* comp)
+    : _comp(comp)
+{}
+
+ai::Compressor::Compressor(ai::eCompressorType type, size_t numThreads)
+    :_type(type)
+{
+    _pool = new ThreadPool(numThreads);
+}
+
+ai::Compressor::~Compressor()
+{
+    if (_pool) {
+        delete _pool;
+    }
+}
+
+std::vector<uint8_t> ai::Compressor::Compress(const std::vector<int>& array) {
+    if (_type == eCompressorType::Uncompressor) {
+        return {};
+    }
+
+    HuffmanCompressorImpl impl(this);
     return impl.Compress(array);
 }
 
-std::vector<int> ai::Compressor::Uncompress(const std::vector<uint8_t>& buffer) const {
-    HuffmanCompressorImpl impl;
+std::vector<int> ai::Compressor::Uncompress(const std::vector<uint8_t>& buffer) {
+    if (_type == eCompressorType::Compressor) {
+        return {};
+    }
+
+    HuffmanCompressorImpl impl(this);
     return impl.Uncompress(buffer);
 }
--- src/Compressor.h
+++ src/Compressor.h
@@ -1,23 +1,54 @@
 #pragma once
 
 #include "Data.h"
+#include "ThreadPool.h"
 
 #include <cstdint>
+#include <vector>
 
 namespace ai
 {
+    class Compressor;
     class CompressorImpl
     {
     public:
-        std::vector<uint8_t> Compress(const std::vector<int>& array) = 0;
-        std::vector<int> UnCompress(uint8_t* buffer) = 0;
+        explicit CompressorImpl(Compressor* comp);
+        virtual ~CompressorImpl() = default;
+
+        virtual std::vector<uint8_t> Compress(const std::vector<int>& array) = 0;
+        virtual std::vector<int> Uncompress(const std::vector<uint8_t>& buffer) = 0;
+
+    protected:
+        Compressor* _comp = nullptr;
     };
+
+
+    enum class eCompressorType : uint8_t {
+        Compressor,
+        Uncompressor
+    };
+
 
     class Compressor
     {
     public:
+        explicit Compressor(eCompressorType type, size_t numThreads = 8);
+        ~Compressor();
+
         std::vector<uint8_t> Compress(const std::vector<int>& array);
         std::vector<int> Uncompress(const std::vector<uint8_t>& buffer);
+
+    private:
+        template <typename Function, typename... Args>
+        auto ScheduleTask(Function&& f, Args&&... args) {
+            if (_pool) {
+                _pool->AddTask(std::forward<Function>(f), std::forward<Args>(args)...);
+            }
+        }
+
+    private:
+        eCompressorType _type;
+        ThreadPool* _pool = nullptr;
 
     private:
         Compressor(const Compressor&) = delete;
