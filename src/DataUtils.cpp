85e977b6330e126cdceeead29dc92d654f9732e8
Aisen Burnashev <oruzejnik@gmail.com>
2023-09-12 18:48:17 +0700

    Moved ai::utils::PrintArray function to header

--- src/DataUtils.cpp
+++ src/DataUtils.cpp
@@ -12,32 +12,3 @@ std::vector<int> ai::utils::CreateAnArray(size_t size) {
 
     return newArray;
 }
-
-template <typename T>
-void ai::utils::PrintArray(const char* nameOfArray, const std::vector<T>& array) {
-    std::cout << nameOfArray << ": ";
-
-    size_t size = array.size();
-    size_t count = 15;
-
-    for (size_t i = 0; i != count; ++i) {
-        assert(i < size);
-        std::cout << array[i] << ", ";
-    }
-
-    std::cout << "....... ";
-
-    for (size_t i = size / 2; i != size / 2 + count; ++i) {
-        assert(i < size);
-        std::cout << array[i] << ", ";
-    }
-
-    std::cout << "....... ";
-
-    for (size_t i = size - count - 1; i != size; ++i) {
-        assert(i < size);
-        std::cout << array[i] << ", ";
-    }
-
-    std::cout << "\n";
-}
--- src/DataUtils.h
+++ src/DataUtils.h
@@ -14,7 +14,37 @@ namespace ai
          * printing first 15, mid 15 and last 15 elements of array
          * */
         template <typename T>
-        void PrintArray(const char* nameOfArray, const std::vector<T>& array);
+        void PrintArray(const char* nameOfArray, const std::vector<T>& array) {
+            std::cout << nameOfArray << ": ";
+
+            size_t size = array.size();
+            if (size == 0) {
+                return;
+            }
+
+            size_t count = 15;
+
+            for (size_t i = 0; i != count; ++i) {
+                assert(i < size);
+                std::cout << array[i] << ", ";
+            }
+
+            std::cout << "....... ";
+
+            for (size_t i = size / 2; i != size / 2 + count; ++i) {
+                assert(i < size);
+                std::cout << array[i] << ", ";
+            }
+
+            std::cout << "....... ";
+
+            for (size_t i = size - count - 1; i != size; ++i) {
+                assert(i < size);
+                std::cout << array[i] << ", ";
+            }
+
+            std::cout << "\n";
+        }
 
     } // utils
 } // ai
